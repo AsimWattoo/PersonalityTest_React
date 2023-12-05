3eda178e1fa657d34020b3d601eedf94805a7600
AsimWattoo <asimwattoo6@gmail.com>
2023-12-05 11:34:33 +0500

    Moved the router from App.tsx to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
+import PageLayout from './PageLayout';
+import NewQuizPage from './pages/NewQuizPage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import PreviewPage from './pages/PreviewPage';
+
+import store, {persistor} from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<NewQuizPage />} />
+            <Route path="/quiz" element={<PageLayout />} />
+            <Route path="/preview" element={<PreviewPage />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
